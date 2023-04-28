765036029d63737d7f9d79ec1aaa0b98612819a2
alpha <alphatero@icloud.com>
2023-04-28 09:38:08 +0800

    feat: 🎸 single queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import type { NextPageWithLayout } from '@/types';
+import { queryClient } from '@/utils/queryClient';
 
 // Props
 type MyAppProps = AppProps & {
   Component: NextPageWithLayout;
 };
-// Create a client
-const queryClient = new QueryClient();
 
 export default function App(Props: MyAppProps) {
   const { Component, pageProps } = Props;

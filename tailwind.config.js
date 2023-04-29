6cf78773c09bec8932b9816f2808f483ffb2efc6
alpha <alphatero@icloud.com>
2023-04-29 23:09:51 +0800

    feat: 🎸 add two type font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,34 @@ module.exports = {
         "noto-sans": ['Noto Sans TC', sans-serif],
       },
       fontSize: {
-        'fs-1': [
+        h1: [
           '40px',
           {
             lineHeight: '48px',
             fontWeight: '700',
           },
-        ]
+        ],
+        h2: [
+          '32px',
+          {
+            lineHeight: '38px',
+            fontWeight: '700',
+          },
+        ],
+        'fs-1': [
+          '40px',
+          {
+            lineHeight: '48px',
+            fontWeight: '400',
+          },
+        ],
+        'fs-2': [
+          '32px',
+          {
+            lineHeight: '40px',
+            fontWeight: '400',
+          }
+        ],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',

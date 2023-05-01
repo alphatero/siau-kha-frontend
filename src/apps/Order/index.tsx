4a798f85e71838d0a9d40f658786cd14c825b933
alpha <alphatero@icloud.com>
2023-05-01 22:00:00 +0800

    feat: 🎸 fixed some format

--- src/apps/Order/index.tsx
+++ src/apps/Order/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPageWithLayout } from '@/types';
-import clsx from 'clsx';
 import { Header, Menu } from './components';
 
 const Order: NextPageWithLayout = () => (
-    <div className={clsx('flex', 'mx-auto w-full')}>
+    <div className="mx-auto flex w-full">
       <Menu />
       <div className="flex-1 items-start">
         <Header />

43f3be5de88260720181eb042aab4a3d288f9a61
alpha <alphatero@icloud.com>
2023-05-21 12:05:58 +0800

    refactor: 💡 tags component

--- src/apps/Order/components/Sidebar.tsx
+++ src/apps/Order/components/Sidebar.tsx
@@ -6,30 +6,29 @@ import { useProducts } from '../hooks/useUpdateProducts';
 
 export const Sidebar = () => {
   const { isLoading, tags } = useProducts();
-  const [currentTag, setCurrentTag] = useState(tags[0].name);
+  const [currentTag, setCurrentTag] = useState(1);
 
-  const onClick = (tag: string) => {
+  const onClick = (tag: number) => {
     setCurrentTag(tag);
   };
 
   return (
     <div className="flex flex-1 flex-col justify-between py-8">
       <ul className="space-y-4 text-h5">
-          {isLoading ? <></>
-            : tags?.map((tag, i) => (
+          {isLoading || tags.map((tag, i) => (
               <li key={i}>
                 <button
                   className={clsx(
                     'flex w-full space-x-3 px-4',
-                    currentTag === tag.name
+                    currentTag === tag.sortNo
                       && 'border-r border-secondary text-secondary',
                   )}
-                  onClick={() => onClick(tag.name)}
+                  onClick={() => onClick(tag.sortNo)}
                 >
                   <span className="w-[7px]">
                     <Icons.Fork
                       className={clsx(
-                        currentTag === tag.name ? 'block' : 'hidden',
+                        currentTag === tag.sortNo ? 'block' : 'hidden',
                       )}
                     />
                   </span>

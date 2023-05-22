76dfd10145906452d54260099d7783cb3bb6f0cc
zoeGuava <zoeguava@gmail.com>
2023-05-22 23:11:37 +0800

    feat: 🎸 add the get promotions api into the hooks/useUpdateList

--- src/apps/Order/hooks/useUpdateList.ts
+++ src/apps/Order/hooks/useUpdateList.ts
@@ -1,17 +1,21 @@
-import { useTable } from '@/services/query';
+import { useTable, usePromotions } from '@/services/query';
 import { useEffect } from 'react';
 import { useStore } from '../stores';
 
 export const useUpdateList = () => {
-  const { setList, setTable } = useStore();
+  const { setList, setTable, setPromotionList } = useStore();
   const { data, isLoading } = useTable();
+  const promotionList = usePromotions().data?.promotions || [];
 
   useEffect(() => {
     if (data) {
       setList(data.list);
       setTable(data.list[0]);
     }
-  }, [data, setList]);
+    if (!!promotionList && promotionList.length > 0) {
+      setPromotionList(promotionList);
+    }
+  }, [data, setList, promotionList]);
 
   return {
     isLoading,

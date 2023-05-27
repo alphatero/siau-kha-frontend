0de0b4d22365e00d1d3fa2126266e5c39f07268e
alpha <alphatero@icloud.com>
2023-05-27 23:19:36 +0800

    fix: 🐛 spacing

--- src/services/query/index.ts
+++ src/services/query/index.ts
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { schema } from './queries/order';
 
-export const useTable = () => useQuery({...schema.table});
-export const useTags = () => useQuery({...schema.tags});
+export const useTable = () => useQuery({ ...schema.table });
+export const useTags = () => useQuery({ ...schema.tags });
 export const usePromotions = () => useQuery({ ...schema.promotions });
 export const useProducts = (tagId: string) => useQuery({ ...schema.products(tagId), enabled: !!tagId });
 

9a8eabe18ff4c065c082092498fc9756165e1106
alpha <alphatero@icloud.com>
2023-05-27 23:14:37 +0800

    feat: 🎸 react query keys schema

--- src/services/mutation/mutations/login.ts
+++ src/services/mutation/mutations/login.ts
@@ -1,10 +1,6 @@
-import { createMutations } from '@/utils/createMutations';
 import * as api from '../api/login';
 
 export const schema = {
-  login: {
-    fn: api.login,
-  },
-} as const;
+  login: api.login,
+} 
 
-export const mutations = createMutations(schema);
--- src/services/query/index.ts
+++ src/services/query/index.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
-import { queries } from './queries/order';
+import { schema } from './queries/order';
 
-export const useTable = () => useQuery(queries.table());
-export const useTags = () => useQuery(queries.tags());
-export const usePromotions = () => useQuery(queries.promotions());
-export const useProducts = (tagId: string) => useQuery({ ...queries.products(tagId), enabled: !!tagId });
+export const useTable = () => useQuery({...schema.table});
+export const useTags = () => useQuery({...schema.tags});
+export const usePromotions = () => useQuery({ ...schema.promotions });
+export const useProducts = (tagId: string) => useQuery({ ...schema.products(tagId), enabled: !!tagId });
 
 export default useTable;

74a52677b80bf3212f6d18af794dea204c6dc0a7
eddie <eddie605134@gmailcom>
2023-06-11 09:01:06 +0800

    feat: 🎸 新增 useSortAndAlertByOrderTime hook

---
+++ src/apps/Kitchen/hooks/useSortAndAlertByOrderTime.ts
@@ -0,0 +1,54 @@
+import {
+  useState, useEffect, useRef,
+} from 'react';
+import dayjs from 'dayjs';
+
+import type { Product, AlertedProduct } from '@/types/kitchen';
+import { AlertType, ProductDetailStatus } from '@/types/kitchen';
+
+function useSortAndAlertByOrderTime(data: Array<Product>): Array<AlertedProduct> {
+  const [sortedData, setSortedData] = useState<Array<AlertedProduct>>([]);
+  const intervalIdRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    const sortAndAlert = () => {
+      const result = [...data].map((item) => {
+        const diffInMinutes = dayjs().diff(dayjs(item.order_time), 'minute');
+        let alertType;
+
+        if (diffInMinutes < 5) {
+          alertType = AlertType.LOW;
+        } else if (diffInMinutes < 10) {
+          alertType = AlertType.MIDDLE;
+        } else {
+          alertType = AlertType.HIGH;
+        }
+
+        return { ...item, alertType };
+      }).sort((a, b) => {
+        if (a.status === ProductDetailStatus.FINISH) {
+          return 1;
+        }
+        if (b.status === ProductDetailStatus.FINISH) {
+          return -1;
+        }
+        return dayjs(a.order_time).unix() - dayjs(b.order_time).unix();
+      });
+
+      if (JSON.stringify(result) !== JSON.stringify(sortedData)) {
+        setSortedData(result);
+      }
+    };
+
+    sortAndAlert(); // 初始的排序和警告級別設置
+    intervalIdRef.current = setInterval(sortAndAlert, 60 * 1000); // 每60秒重新計算一次
+
+    return () => {
+      clearInterval(intervalIdRef.current); // 防止內存溢出
+    };
+  }, [data]); // 每當rawData變化時，都會重新設置間隔
+
+  return sortedData;
+}
+
+export default useSortAndAlertByOrderTime;

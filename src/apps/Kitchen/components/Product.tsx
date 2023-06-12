78d6a8382fee52536229fb19c503d6c8fa8981ba
eddie.chang <eddie605134@gmail.com>
2023-06-12 19:52:18 +0800

    fix: 🐛 product-detail-status狀態用enum替代

--- src/apps/Kitchen/components/Product.tsx
+++ src/apps/Kitchen/components/Product.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { AlertType } from '@/types/kitchen';
+import { AlertType, ProductDetailStatus } from '@/types/kitchen';
 
 type Props = {
   productName: string,
@@ -22,8 +22,8 @@ export const Product = (props: Props) => {
     <div
     className={clsx(
       'rounded border px-3 py-2',
-      status !== 'FINISH' && alertType === AlertType.HIGH ? ' border-warn' : 'border-black/10 ',
-      status === 'FINISH' && 'bg-black/10',
+      status !== ProductDetailStatus.FINISH && alertType === AlertType.HIGH ? ' border-warn' : 'border-black/10 ',
+      status === ProductDetailStatus.FINISH && 'bg-black/10',
     )}>
       <div className='flex items-center justify-between mb-4'>
         <div className='flex items-baseline space-x-1'>
@@ -36,25 +36,25 @@ export const Product = (props: Props) => {
           className={
             clsx(
               'text-fs-7 text-info',
-              status !== 'FINISH' ? {
+              status !== ProductDetailStatus.FINISH ? {
                 'text-warn': alertType === AlertType.HIGH,
                 'text-primary': alertType === AlertType.MIDDLE,
                 'text-secondary': alertType === AlertType.LOW,
               } : {},
-              status === 'FINISH' ? 'text-black/85' : 'text-info',
+              status === ProductDetailStatus.FINISH ? 'text-black/85' : 'text-info',
             )}>{orderTime}</span>
       </div>
       <button className={clsx(
         'w-full rounded-s py-1',
-        status !== 'FINISH' ? {
+        status !== ProductDetailStatus.FINISH ? {
           'bg-warn': alertType === AlertType.HIGH,
           'bg-primary': alertType === AlertType.MIDDLE,
           'bg-secondary': alertType === AlertType.LOW,
         } : {},
-        status === 'FINISH' ? 'bg-black/25 text-black/85' : 'bg-primary text-white',
+        status === ProductDetailStatus.FINISH ? 'bg-black/25 text-black/85' : 'bg-primary text-white',
       )}
       >
-        <span className="text-fs-6">{`${status === 'IN_PROGRESS' ? '出菜' : '已完成'}`}</span>
+        <span className="text-fs-6">{`${status === ProductDetailStatus.IN_PROGRESS ? '出菜' : '已完成'}`}</span>
       </button>
     </div>
   );

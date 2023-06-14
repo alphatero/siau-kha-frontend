a04b3b6da6f85e6a11a8b2b3acb9f0873eea8075
amy <amy>
2023-06-14 23:23:07 +0800

    fix: 🐛 remove useEffect, use filter to get now table info

--- src/apps/Counter/components/Modals/TableMeal.tsx
+++ src/apps/Counter/components/Modals/TableMeal.tsx
@@ -3,9 +3,8 @@ import { Button, TextField } from '@/components/common';
 import { useForm } from 'react-hook-form';
 import { useModalStore } from '@/stores/modal';
 import { usePatchTable } from '@/services/mutation';
-import { TableStatus, TableType } from '@/types/order';
+import { TableStatus } from '@/types/order';
 import { HttpStatusCode } from 'axios';
-import { useEffect, useState } from 'react';
 import { useStore } from '../../stores';
 import { useUpdateList } from '../../hooks/useUpdateList';
 
@@ -18,17 +17,12 @@ export const TableMeal = () => {
   const { selectedTable, list } = useStore();
   const { mutateAsync } = usePatchTable();
   const { refetch } = useUpdateList();
-  const [table, setTable] = useState<TableType>();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<OnMealType>();
-
-  useEffect(() => {
-    setTable(list.find((t) => t.id === selectedTable));
-  }, []);
 
   const onSubmit = async (data: OnMealType) => {
     const res = await mutateAsync({ id: selectedTable, status: TableStatus.MEAL, customerNum: Number(data.customerNum) });
@@ -40,44 +34,50 @@ export const TableMeal = () => {
   };
   return (
     <div className="flex flex-1 flex-col space-y-6">
-      <h5
-        className={clsx(
-          'w-full border-b border-black/10',
-          'pb-4 text-h5',
-        )}
-      >
-        {table?.name}
-        <span
-          className={clsx(
-            'rounded-md border border-info text-fs-6 text-info',
-            'ms-2 px-2 py-1',
-          )}
-        >
-            用餐
-        </span>
-      </h5>
+      {
+        list.filter((t) => t.id === selectedTable).map((table) => (
+          <>
+            <h5
+              className={clsx(
+                'w-full border-b border-black/10',
+                'pb-4 text-h5',
+              )}
+            >
+              {table?.name}
+              <span
+                className={clsx(
+                  'rounded-md border border-info text-fs-6 text-info',
+                  'ms-2 px-2 py-1',
+                )}
+              >
+                  用餐
+              </span>
+            </h5>
 
-      <form
-        className="flex h-full flex-1 flex-col justify-between"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className='text-black/85'>
+            <form
+              className="flex h-full flex-1 flex-col justify-between"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className='text-black/85'>
 
-          <TextField label="請輸入用餐人數" type="number" placeholder='輸入人數'
-            {...register('customerNum', { required: '此為必填', min: { value: 1, message: '至少1位' }, max: { value: table?.seat ? table.seat + 2 : 0, message: `最多${table?.seat ? table.seat + 2 : 0}位` } })} />
-          <p className='mt-1 text-warn'>{errors.customerNum?.message}</p>
+                <TextField label="請輸入用餐人數" type="number" placeholder='輸入人數'
+                  {...register('customerNum', { required: '此為必填', min: { value: 1, message: '至少1位' }, max: { value: table.seat ? table.seat + 2 : 0, message: `最多${table.seat ? table.seat + 2 : 0}位` } })} />
+                <p className='mt-1 text-warn'>{errors.customerNum?.message}</p>
 
-        </div>
+              </div>
 
-        <div className="flex justify-between space-x-4">
-          <Button type="reset" className="w-full" outline color="gray" onClick={() => setIsOpen(false)}>
-            取消
-          </Button>
-          <Button type="submit" className="w-full">
-            儲存
-          </Button>
-        </div>
-      </form>
+              <div className="flex justify-between space-x-4">
+                <Button type="reset" className="w-full" outline color="gray" onClick={() => setIsOpen(false)}>
+                  取消
+                </Button>
+                <Button type="submit" className="w-full">
+                  儲存
+                </Button>
+              </div>
+            </form>
+          </>
+        ))
+      }
     </div>
   );
 };

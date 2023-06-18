c192dd10588fd98b24a6b0f3b6a221f04d82f7b8
zoeGuava <zoeguava@gmail.com>
2023-06-18 16:19:49 +0800

    feat: 🎸 when the meal isDelete, then display another status

--- src/apps/Order/components/Modals/LogButtons.tsx
+++ src/apps/Order/components/Modals/LogButtons.tsx
@@ -18,24 +18,33 @@ export const LogButtons = (props: Props) => {
       'flex w-full items-center justify-between',
       'space-x-4 whitespace-nowrap',
     )}>
-      <Button
-        onClick={removeItem}
-        color={isDelete ? 'gray' : 'warn'}
-        disabled={status !== ProductDetailStatus.SUCCESS}
-      >退點</Button>
       {
-        status === ProductDetailStatus.IN_PROGRESS ? (
-          <Button
-            color="secondary"
-            outline
-            disabled
-          >製作中</Button>
-        ) : (
-          <Button
-            color="primary"
-            outline={status === ProductDetailStatus.SUCCESS}
-            disabled={status === ProductDetailStatus.SUCCESS}
-          >{status === ProductDetailStatus.SUCCESS ? '已上菜' : '上菜'}</Button>
+        isDelete && <div className='ml-auto rounded-md bg-black/50 px-2 py-1 text-white'>已退點</div>
+      }
+      {
+        isDelete || (
+          <>
+            <Button
+              onClick={removeItem}
+              color={status === ProductDetailStatus.SUCCESS ? 'gray' : 'warn'}
+              disabled={status === ProductDetailStatus.SUCCESS}
+            >退點</Button>
+            {
+              status === ProductDetailStatus.IN_PROGRESS ? (
+                <Button
+                  color="secondary"
+                  outline
+                  disabled
+                >製作中</Button>
+              ) : (
+                <Button
+                  color="primary"
+                  outline={status === ProductDetailStatus.SUCCESS}
+                  disabled={status === ProductDetailStatus.SUCCESS}
+                >{status === ProductDetailStatus.SUCCESS ? '已上菜' : '上菜'}</Button>
+              )
+            }
+          </>
         )
       }
     </div>
